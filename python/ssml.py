b987fb4466a60e065a4e97c8cfe14f099da94517
Gabe Schamberg <gschambe@eng.ucsd.edu>
2017-10-05 12:54:52 -0700

    Added parameter to enable ssml for sparse variations

--- python/ssml.py
+++ python/ssml.py
@@ -6,17 +6,28 @@
 
 import numpy as np
 from framework import AdmmObject, AdmmSolver
-from updates import ssml_x, ssml_x_grad, ssml_x_parallel, ssml_x_grad_parallel, vector_z, least_sq_w
+from updates import ssml_x, ssml_x_grad, ssml_x_parallel, ssml_x_grad_parallel, vector_z, least_sq_w, l1_w
 from updates import vector_lam, vector_alph
 
 
 def ssml(observations, rho, params,
-         thresh=0.0001, max_iters=10, verbosity=0, history=False, parallel=False, approx=False):
+         thresh=0.0001, max_iters=10, verbosity=0,
+         history=False, parallel=False, approx=False,
+         sparse=False):
     # Number of trials
     N = len(observations[0])
-    admmobj = AdmmObject(N, observations, rho,
-                         ssml_x_parallel, vector_z, least_sq_w,
-                         vector_lam, vector_alph)
+    if sparse:
+        admmobj = AdmmObject(N, observations, rho,
+                             ssml_x_parallel, vector_z,
+                             l1_w, vector_lam,
+                             vector_alph)
+        admmobj.beta = params[13]
+    else:
+        admmobj = AdmmObject(N, observations, rho,
+                             ssml_x_parallel, vector_z,
+                             least_sq_w, vector_lam,
+                             vector_alph)
+        admmobj.beta = 1
     # Augmented Lagrange parameter
     admmobj.rho = rho
     # A bunch of Parameters for ssml problem
@@ -33,7 +44,6 @@ def ssml(observations, rho, params,
     admmobj.c = params[10]
     admmobj.Del = params[11]
     admmobj.J = params[12]
-    admmobj.beta = 1  # Determined by SSML problem
     # x - Estimate of learning state
     admmobj.x = np.zeros((admmobj.N,))
     # z - enforces relationship between x and w
@@ -43,11 +53,18 @@ def ssml(observations, rho, params,
     admmobj.lam = np.ones((admmobj.N,))
     admmobj.alph = np.ones((admmobj.N,))
     # Extra variables used in z update
-    G = np.eye(admmobj.N)
-    for i in range(1, admmobj.N):
-        G[i, i - 1] = -admmobj.phi
-    admmobj.igginv = np.linalg.inv(np.eye(admmobj.N) + np.dot(G.transpose(), G))
-    admmobj.G = G
+    if sparse:
+        G = np.eye(admmobj.N)
+        for i in range(1, admmobj.N):
+            G[i, i - 1] = -1
+        admmobj.igginv = np.linalg.inv(np.eye(admmobj.N) + np.dot(G.transpose(), G))
+        admmobj.G = G
+    else:
+        G = np.eye(admmobj.N)
+        for i in range(1, admmobj.N):
+            G[i, i - 1] = -admmobj.phi
+        admmobj.igginv = np.linalg.inv(np.eye(admmobj.N) + np.dot(G.transpose(), G))
+        admmobj.G = G
 
     admmslv = AdmmSolver(admmobj, thresh, max_iters, verbosity, history)
     (x, w, z, lam, alph) = admmslv.solve()
